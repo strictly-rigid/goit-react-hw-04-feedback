1eac1f5ad9291b0b70fde44c7b776b229577191e
strictly-rigid <holysociology@gmail.com>
2023-08-03 18:23:47 +0300

    refactored using hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,48 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from '../components/Section/Section';
 import css from './App.module.css';
 
-class App extends Component {
-  state = {
+export default function App() {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const countTotalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positivePercentage = (
+    (feedback.good / countTotalFeedback) *
+    100
+  ).toFixed(2);
+
+  const onLeaveFeedBack = item => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [item]: prevFeedback[item] + 1,
+    }));
   };
 
-  countTotalFeedback = () =>
-    this.state.good + this.state.neutral + this.state.bad;
-
-  countPositiveFeedbackPercentage = () =>
-    ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
-
-  onLeaveFeedBack = item => {
-    this.setState(prevState => {
-      return { [item]: prevState[item] + 1 };
-    });
-  };
-
-  render() {
-    return (
-      <div className={css.container}>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedBack={this.onLeaveFeedBack}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </div>
-    );
-  }
+  return (
+    <div className={css.container}>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedBack={onLeaveFeedBack}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={countTotalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      </Section>
+    </div>
+  );
 }
-
-export default App;
